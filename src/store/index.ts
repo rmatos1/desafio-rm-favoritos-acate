a2aef3cf2d9c37b4c27b8489439bf4f30e1d05b6
Robério <roberiomatos@yahoo.com.br>
2022-08-26 21:41:37 -0300

    updated card, reducer, types

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,4 @@
-import {
-    configureStore
-} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
 import FavoritesReducer from './modules/favorites/reducer';
 
--- src/store/modules/favorites/reducer.ts
+++ src/store/modules/favorites/reducer.ts
@@ -1,7 +1,4 @@
-import {
-    createSlice,
-    PayloadAction
-} from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import { IFavorites, ICharacter } from '../../../types';
 
